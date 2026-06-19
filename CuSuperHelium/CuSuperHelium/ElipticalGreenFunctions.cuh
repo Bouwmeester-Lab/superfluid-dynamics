9fa72fb9dfc1560a817167b5a0f6089acb157643
emorell96 <emorell96@hotmail.com>
2026-06-19 19:28:32 +0200

    Refactor elliptic integral usage and add calculate_mc
    Replaced elliptic_K_parameter with elliptic_K_E_parameter_from_m_mc and elliptic_dKdm_parameter. Marked all ElipticalGreenFunctions methods as inline. Added calculate_mc for improved numerical stability. Moved function implementations to the header and added calculate_dGdrho using the new approach.

--- CuSuperHelium/CuSuperHelium/ElipticalGreenFunctions.cuh
+++ CuSuperHelium/CuSuperHelium/ElipticalGreenFunctions.cuh
@@ -6,52 +6,30 @@
 #include <cmath>
 #include <limits>
 #include "constants.cuh"
-
-__host__ __device__
-double elliptic_K_parameter(double m)
-{
-    // K(m) = int_0^{pi/2} dtheta / sqrt(1 - m sin^2 theta)
-    // valid for 0 <= m < 1
-
-    if (m < 0.0) return NAN;
-    if (m == 1.0) return INFINITY;
-    if (m > 1.0) return NAN;
-
-    double a = 1.0;
-    double b = sqrt(1.0 - m);
-
-    // AGM iteration
-    for (int i = 0; i < 20; ++i)
-    {
-        double an = 0.5 * (a + b);
-        double bn = sqrt(a * b);
-
-        if (fabs(an - bn) <= 4.0 * std::numeric_limits<double>::epsilon() * an)
-        {
-            a = an;
-            break;
-        }
-
-        a = an;
-        b = bn;
-    }
-
-    return PI_d / (2.0 * a);
-}
+#include "ElipticalIntegrals.cuh"
 
 class ElipticalGreenFunctions
 {
 public:
 	ElipticalGreenFunctions();
 	~ElipticalGreenFunctions();
-	__device__ __host__ double calculate_A(double r, double z, double rho, double eta);
-	__device__ __host__ double calculate_m(double r, double z, double rho, double eta);
+	__device__ __host__ __inline__ double calculate_A(double r, double z, double rho, double eta);
+	__device__ __host__ __inline__ double calculate_m(double r, double z, double rho, double eta);
+	/// <summary>
+	/// Calculates the complementary m: mc = 1 - m from the analytical expression. This is used to avoid numerical issues when m is close to 1.
+	/// </summary>
+	/// <param name="r"></param>
+	/// <param name="z"></param>
+	/// <param name="rho"></param>
+	/// <param name="eta"></param>
+	/// <returns></returns>
+	__device__ __host__ __inline__ double calculate_mc(double r, double z, double rho, double eta);
 	// derivatives
-	__device__ __host__ double calculate_dmdrho(double r, double z, double rho, double eta);
-	__device__ __host__ double calculate_dmdeta(double r, double z, double rho, double eta);
+	__device__ __host__ __inline__ double calculate_dmdrho(double r, double z, double rho, double eta);
+	__device__ __host__ __inline__ double calculate_dmdeta(double r, double z, double rho, double eta);
 
-	__device__ __host__ double calculate_dGdrho(double r, double z, double rho, double eta);
-	__device__ __host__ double calculate_dGdeta(double r, double z, double rho, double eta);
+	__device__ __host__ __inline__ double calculate_dGdrho(double r, double z, double rho, double eta);
+	__device__ __host__ __inline__ double calculate_dGdeta(double r, double z, double rho, double eta);
 
 private:
 
@@ -63,22 +41,44 @@ ElipticalGreenFunctions::ElipticalGreenFunctions()
 ElipticalGreenFunctions::~ElipticalGreenFunctions()
 {}
 
-__device__ __host__ double ElipticalGreenFunctions::calculate_A(double r, double z, double rho, double eta)
+__device__ __host__ __inline__ double ElipticalGreenFunctions::calculate_A(double r, double z, double rho, double eta)
 {
 	return sqrt(pow(r + rho, 2.0) + pow(z - eta, 2.0));
 }
 
-__device__ __host__ double ElipticalGreenFunctions::calculate_m(double r, double z, double rho, double eta)
+__device__ __host__ __inline__ double ElipticalGreenFunctions::calculate_m(double r, double z, double rho, double eta)
 {
 	return 4.0 * r * rho / (pow(r + rho, 2.0) + pow(z - eta, 2.0));
 }
 
-__device__ __host__ double ElipticalGreenFunctions::calculate_dmdrho(double r, double z, double rho, double eta)
+__device__ __host__ __inline__ double ElipticalGreenFunctions::calculate_mc(double r, double z, double rho, double eta)
+{
+	return (pow(r - rho, 2.0) + pow(z - eta, 2.0)) / (pow(r + rho, 2.0) + pow(z - eta, 2.0));
+}
+
+__device__ __host__ __inline__ double ElipticalGreenFunctions::calculate_dmdrho(double r, double z, double rho, double eta)
 {
 	return 4.0*r*rho*(z-eta)/pow(pow(r+rho, 2.0) + pow(z-eta, 2.0), 2.0);
 }
 
-__device__ __host__ double ElipticalGreenFunctions::calculate_dmdeta(double r, double z, double rho, double eta)
+__device__ __host__ __inline__ double ElipticalGreenFunctions::calculate_dmdeta(double r, double z, double rho, double eta)
 {
 	return 8 * r * rho * (z - eta) / pow(pow(r + rho, 2.0) + pow(z - eta, 2.0), 2.0);
+}
+
+__device__ __host__ __inline__ double ElipticalGreenFunctions::calculate_dGdrho(double r, double z, double rho, double eta)
+{
+	double A = calculate_A(r, z, rho, eta);
+	double one_over_A = 1.0 / A;
+	double m = calculate_m(r, z, rho, eta);
+	double mc = calculate_mc(r, z, rho, eta);
+
+	double dmdrho = calculate_dmdrho(r, z, rho, eta);
+
+	double K, E;
+	elliptic_K_E_parameter_from_m_mc(m, mc, &K, &E);
+	double dKdm = elliptic_dKdm_parameter(m, mc, K, E);
+
+
+	return 0.5 * CUDART_2_OVER_PI * (-(r + rho) / pow(A, 3.0) * K + one_over_A * dmdrho * dKdm);
 }
